8cf5eb17cc920f90b23417b3a0b1f9127df7f7d9
Anton <zhl96@mail.ru>
2023-05-24 14:56:41 +0300

    initial

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const credentials = require("./credentials.json");
-
+require("dotenv").config();
 const FeedSub = require("feedsub");
-const rssFeed = credentials.rss_feed;
+const rssFeed = process.env.rss_feed;
 let reader = new FeedSub(rssFeed, {
   interval: 1, // Check feed every 1 minute.
 });
 
+console.log(process.env);
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
@@ -16,13 +16,13 @@ db.defaults({ feed: [] }).write();
 const Telegraf = require("telegraf");
 const Extra = require("telegraf/extra");
 const session = require("telegraf/session");
-const token = credentials.telegram_bot_token;
+const token = process.env.telegram_bot_token;
 const bot = new Telegraf(token);
 
 // Register session middleware.
 bot.use(session());
 
-bot.telegram.sendMessage(credentials.telegram_channel, "Bot started");
+bot.telegram.sendMessage(process.env.telegram_channel, "Bot started");
 
 // Register logger middleware.
 bot.use((ctx, next) => {
@@ -51,7 +51,7 @@ reader.on("item", (item) => {
     }
 
     bot.telegram.sendMessage(
-      credentials.telegram_channel,
+      process.env.telegram_channel,
       message,
       Extra.HTML().markup()
     );
