59fd8590981202abcd7659904c3b4676986c5325
Anton <zhl96@mail.ru>
2023-05-25 10:08:40 +0300

    FIX: typo

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ const express = require("express");
 
 // Constants
 const PORT = process.env.PORT || 8000;
-const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT} port`);
 });
 
 const low = require("lowdb");
