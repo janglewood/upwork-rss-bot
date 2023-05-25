c7213a3df94df3b7c827e54404e97811980e18d9
Anton <zhl96@mail.ru>
2023-05-25 09:02:05 +0300

    FIX: typo

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const port = 8000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
